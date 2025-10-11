494c033d78167d1febe4365569e1260eebbc620a
ryder0930 <99664490+ryder0930@users.noreply.github.com>
2025-10-12 07:02:19 +0800

    AAA

--- lagertool.py
+++ lagertool.py
@@ -3,8 +3,29 @@ import sqlite3
 
 # sector for User
 class User:
+    # define vaiables in the object
     def __init__(self) -> None:
-        self.catagory = ""
+        self.user_info=[]
+        self.type=-1
+        self.other_info=[]
+        self.other_info_index=-1
+        self.temp=0
+    
+    # functions in the class for accessing the object
+    
+    # to initialize the user when logged in
+    def initialize(self, user_id, cat):
+        self.user_info=[]
+        self.type=-1
+        self.other_info=[]
+        self.other_info_index=-1
+        self.temp=0
+        self.user_info.append(user_id)
+
+    # temporary store for the other information that is needed
+    def save_other(self, val):
+        self.other_info.append(val)
+        self.other_info_index+=1
 
 # initialize Flask app
 app = Flask(__name__)
@@ -38,19 +59,21 @@ def login():
         if request.form.get('submit') == 'LOGIN':
             userN = request.form.get('username')
             passW = request.form.get('password')
+            userID = -1
             userID = UserCheck(userN, passW)
             if userID != -1:
                 conn = sqlite3.connect("Lagertur.db")
                 cur = conn.cursor()
                 cur.execute("SELECT catagory FROM login WHERE userID='{}'".format(userID))
-                user01.catagory = cur.fetchone()[0]
+                cat=cur.fetchone()
+                user01.initialize(userID, cat[0])
                 conn.close()
-                return redirect(url_for('myBorrow'))
+                return redirect(url_for('mybookings'))
     return render_template('login.html')
 
 # route for My Bookings
 @app.route('/mybookings')
-def myBorrow():
+def mybookings():
     conn = sqlite3.connect("Lagertur.db")
     cur = conn.cursor()
     
